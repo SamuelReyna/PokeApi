b81fa0cc0ad9e503ecdca0ae17af30403d7f6a01
Alien 15 <Alien 15@192.167.0.119>
2025-10-29 17:49:43 -0600

    Creacion de entidad Pokemon y paquete de Modelo.

---
+++ src/main/java/risosu/it/PokeApiClient/ML/Cries.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package risosu.it.PokeApiClient.ML;
+
+/**
+ *
+ * @author Alien 15
+ */
+public class Cries {
+    private byte[] latest;
+    private byte [] legacy;
+    
+    
+
+    public byte[] getLatest() {
+        return latest;
+    }
+
+    public void setLatest(byte[] latest) {
+        this.latest = latest;
+    }
+
+    public byte[] getLegacy() {
+        return legacy;
+    }
+
+    public void setLegacy(byte[] legacy) {
+        this.legacy = legacy;
+    }
+    
+    
+}
---
+++ src/main/java/risosu/it/PokeApiClient/ML/Pokemon.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package risosu.it.PokeApiClient.ML;
+
+import java.util.List;
+
+/**
+ *
+ * @author Alien 15
+ */
+public class Pokemon {
+    
+    
+    
+    private String  name;
+    
+    private int order; //Pokemons del mismo orden.
+    
+    private String height; //Altura del pokemon expresada en decimetros 1dc = 10cm 
+    
+    private String weight; //Peso de pokemon expresado en hectogramos 1hectogramo = 100 gramos
+    
+    private boolean is_default; //Si el pokemon se encuentra en su forma base (No evolucionado )
+
+    private List<Sprite> sprites ; //Imagenes del pokemon 
+ 
+    private List<Cries> cries ; //Efectos de sonido
+    
+    public Pokemon(){};
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public boolean isIs_default() {
+        return is_default;
+    }
+
+    public void setIs_default(boolean is_default) {
+        this.is_default = is_default;
+    }
+
+    public List<Sprite> getSprites() {
+        return sprites;
+    }
+
+    public void setSprites(List<Sprite> sprites) {
+        this.sprites = sprites;
+    }
+
+    public List<Cries> getCries() {
+        return cries;
+    }
+
+    public void setCries(List<Cries> cries) {
+        this.cries = cries;
+    }
+   
+    
+}
