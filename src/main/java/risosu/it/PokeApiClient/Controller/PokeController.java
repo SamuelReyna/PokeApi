c6c92cf459e45eb689fe0dab13e57c74be3f38a5
Alien 15 <Alien 15@192.167.1.155>
2025-11-04 13:07:49 -0600

    Redireccionamiento a vista automatico.

--- src/main/java/risosu/it/PokeApiClient/Controller/PokeController.java
+++ src/main/java/risosu/it/PokeApiClient/Controller/PokeController.java
@@ -5,59 +5,59 @@
 package risosu.it.PokeApiClient.Controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import risosu.it.PokeApiClient.DTO.PokeDetailDTO;
 import risosu.it.PokeApiClient.DTO.PokemonDTO;
+import risosu.it.PokeApiClient.ML.Pokemon;
 import risosu.it.PokeApiClient.ML.Result;
 
 
 @Controller
-@RequestMapping("/pokeInicio")
+@RequestMapping("/pokeControl")
 public class PokeController {
-    
-//    @Autowired
-//    private PokeService pokeService;
-//    
-//   @GetMapping()
-//public String Inicio(Model model) {
-//
-//    Result result = pokeService.PokeLista();
-//
-//    if (!result.correct) {
-//        model.addAttribute("error", "No se pudo obtener la lista de Pokémon");
-//        return "error";
-//    }
-//
-//    ObjectMapper mapper = new ObjectMapper();
-//    List<LinkedHashMap<String, Object>> rawList = (List<LinkedHashMap<String, Object>>) result.object;
-//    List<PokemonDTO> pokemones = new ArrayList<>();
-//
-//    for (LinkedHashMap<String, Object> raw : rawList) {
-//        PokemonDTO dto = mapper.convertValue(raw, PokemonDTO.class);
-//        pokemones.add(dto);
-//    }
-//
-//    
-//    // Ahora agregamos el detail a cada uno
-//    for (PokemonDTO pokemon : pokemones) {
-//        Result resultDetail = pokeService.PokeDetail(pokemon.getUrl());
-//        if (resultDetail.correct && resultDetail.object != null) {
-//            PokeDetailDTO detail = mapper.convertValue(resultDetail.object, PokeDetailDTO.class);
-//            pokemon.setDetail(detail);
-//        }
-//    }
-//
-//    model.addAttribute("pokemons", pokemones);
-//    
-//    return "index";
-//}
+     //Este controlador es accedido desde vista "loading" para validar si existe ya el archivo JSON y redirigir:
+    @GetMapping("/status")
+    @ResponseBody
+    public Map<String, Object> verificarEstado() {
+        File archivo = new File("pokemons.json");
+        Map<String, Object> estado = new HashMap<>();
+        estado.put("ready", archivo.exists());
+        return estado;
+    }
+
+        
+        //Este controlador se encarga de mostrar la lista de pokemons:
+        
+        @GetMapping("/listar")
+        public String mostrarVistaFinal(Model model) {
+        // Leer el archivo JSON
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            List<Pokemon> pokemones = Arrays.asList(
+                mapper.readValue(new File("pokemons.json"), Pokemon[].class)
+            );
+            model.addAttribute("pokemones", pokemones);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+            model.addAttribute("error", "No se pudo leer el archivo");
+        }
+
+        return "index"; // Thymeleaf buscará vistaFinal.html
+    }
 }
 
