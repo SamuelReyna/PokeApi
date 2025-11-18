718319e4325dc86bb78a9d9fa6a7a5f9157e3f39
digis <digis@192.167.1.246>
2025-11-18 12:37:30 -0600

    vista de pokemones favoritos

--- src/main/java/risosu/it/PokeApiClient/Controller/PokeController.java
+++ src/main/java/risosu/it/PokeApiClient/Controller/PokeController.java
@@ -1,4 +1,4 @@
- ///*
+     ///*
 // * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
 // * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
 // */
@@ -204,7 +204,7 @@ public class PokeController {
 
             System.out.println("Pokemones mapeados:");
             System.out.println(pokemons);
-
+            model.addAttribute("pokedex", pokedex);
             model.addAttribute("pokemones", pokemons);
             model.addAttribute("entrenador", entrenador);
         }
